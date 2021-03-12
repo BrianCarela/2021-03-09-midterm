a14b49e39cb017575368719b65e1d7372250e828
BrianCarela <briancarela@gmail.com>
2021-03-12 15:09:46 -0500

    another solution

--- main.js
+++ main.js
@@ -1,81 +1,85 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
-function ageToAbilities(num){
-    if (num < 16){
-        return "You can't drive."
+function ageToAbilities(age){
+    if(age < 16){
+        return "You can't drive.";
+    }else if(age === 16 || age === 17){
+        return "You can drive but not vote.";
+    }else if(age >= 18 && age <= 24){
+        return "You can vote but not rent a car.";
+    }else{
+        return "You can do pretty much anything.";
     }
-   if (num >= 16 && num <= 17){
-       return "You can drive but not vote.";
-   } if (num >= 18 && num <= 24){
-       return "You can vote but not rent a car."
-   } else {
-       return "You can do pretty much anything.";
-   }
 }
 
 function oddIndices(arr){
-    newArr = [];
-    for (i = 0; i < arr.length; i++){
-        if (i % 2 !== 0){
-            newArr.push(arr[i]);
-        }
+    let output = [];
+    for (let i = 1; i < arr.length; i += 2){
+        output.push(arr[i]);
     }
-    return newArr;
+    return output;
 }
 
 function numOddValues(arr){
-    oddNumbers = 0;
-    for (index of arr) {
-        if( index % 2 !== 0){
-            oddNumbers++;
+    let output = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] % 2 !== 0){
+            output++;
         }
     }
-    return oddNumbers;
+    return output;
 }
 
-function averageStringLength(arr) {
-	 str = 0;
-	if (arr.length === 0) {
-		return 0;
-	} else {
-		for (index of arr) {
-			str += index.length;
-		}
-		return str / arr.length;
- 	}
- }
+function averageStringLength(arr){
+    if(arr.length === 0){
+        return 0; 
+    }
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i].length;
+    }
+    return sum/arr.length;
+}
 
- function firstPunctuationIndex(str) {
-    for (char of str) {
-        if (char === "." || char === "!" || char === "?") {
-            return str.indexOf(char);
+function firstPunctuationIndex(str){
+    for (let i = 0; i < str.length; i++){
+        if (str[i] === "."){
+            return str.indexOf(".");
+        }else if (str[i] === "?"){
+            return str.indexOf("?");
+        }else if (str[i] === "!"){
+            return str.indexOf("!");
         }
     }
     return -1;
 }
 
-function getPlace(score, newScore) {
-    let final = 0;
-    score.push(newScore);
-    score.sort(function(a,b){return b-a});
-    for (let i = 0; i < score.length; i++) {
-        if (score[i] === newScore) {
-            final = i + 1;
+function getPlace(scoreArr, score){
+    let str;
+    for (let i = 0; i < scoreArr.length; i++){
+        if (score > scoreArr[i]){
+            str = i+1;
+            break;
         }
     }
-    if (final.toString().substr(-1) == "1") {
-        return final + "st place";
-    } else if (final.toString().substr(-1) === "2") {
-        return final + "nd place";
-    } else if (final.toString().substr(-1) === "3") {
-        return final + "rd place";
-    } else if (final.toString().substr(-1) === "4" || final.toString().substr(-1) === "5" || final.toString().substr(-1) === "6" || final.toString().substr(-1) === "7" || final.toString().substr(-1) === "8" || final.toString().substr(-1) === "9") {
-        return final + "th place";
+    if(!str){
+        str = scoreArr.length+1;
+    }
+    if(str >= "11" && str <= 13){
+        return str + "th place";
+    }
+    let human = str.toString().split("");
+    if (human[human.length-1] === "1"){
+        return str + "st place";
+    }else if (human[human.length-1] === "2"){
+        return str + "nd place";
+    }else if (human[human.length-1] === "3"){
+        return str + "rd place";
+    }else{
+        return str + "th place";
     }
 }
-
-
 
 
 // ┌─────────────────────────────────────┐
